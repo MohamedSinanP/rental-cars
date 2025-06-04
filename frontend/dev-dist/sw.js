47b06ea506179049af7314e1d7c321d1c12c9981
MohamedSinanP <mohamedsinanp8@gmail.com>
2025-06-04 21:52:38 +0530

    updated service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6f5e3fbb'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0slh8firus"
+    "revision": "0.luni61gacmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
